75f0b85108815f150648b14f9f34a2fc27200f8e
Aby Leyva <abyleyva75@gmail.com>
2026-06-09 22:03:05 -0600

    Initial commit
     Conflicts:

--- 01_variables.php
+++ 01_variables.php
@@ -1,25 +1,21 @@
 <?php
-// HomeWork 1:
-// add 2 variables: Price and Publis Date
-$price=9.99;
-$publishDate="2024-06-01";
-
-
-$courserName = "PHP Fundamentals part 1";
+    $course = "PHP Fundamentals First Steps";
+    $price = 19.99;
+    $published_date = "2024-06-01";
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=  $courserName; ?></title>
+    <title><?= $course; ?></title>
 </head>
 <body>
-    <h1>Welcome to <?= $courserName; ?></h1>
-    <p>In this course, we will learn the basics of PHP programming language.</p>
-
-    <p>The course <?= $courserName ?> have a price of <span><?= $price ?></span>
-     and was Published at: <?= $publishDate ?>
-    </p>
+    <h1>Welcome to <?= $course; ?></h1>
+    <p>Price: $<?= $price; ?></p>
+    <p>Published on: <?= $published_date; ?></p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque similique, reprehenderit facilis error perferendis explicabo quam, consequuntur commodi placeat porro officia quas possimus et. Nam facere dolores consectetur voluptatem quasi?
+    Cupiditate, expedita. Fuga a error architecto. Asperiores id a cum dolor saepe, vero quam blanditiis tempora beatae doloremque incidunt earum quae? Mollitia amet reprehenderit at molestias ex accusamus sunt quisquam.
+    Cupiditate quibusdam iure vitae commodi reiciendis qui sint sit, ipsa et in labore ratione non cum dolore! Voluptates, iure quo? Reprehenderit in dolorum animi mollitia, voluptates expedita vel? Doloremque, libero.</p>
 </body>
 </html>
